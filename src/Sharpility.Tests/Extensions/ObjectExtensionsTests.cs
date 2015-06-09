7d6a9345e79fd78d7ce4aaa027d6afa5bda052b3
mstefaniuk <marcin.stefaniuk@finder.pl>
2015-06-09 09:59:22 +0200

    Funny tests.

--- src/Sharpility.Tests/Extensions/ObjectExtensionsTests.cs
+++ src/Sharpility.Tests/Extensions/ObjectExtensionsTests.cs
@@ -1,8 +1,38 @@
 ﻿
+using NUnit.Framework;
+using NFluent;
+using Sharpility.Extensions;
+using System;
+using Sharpility.Util;
+using Moq;
+
 namespace Sharpility.Tests.Extensions
 {
+    [TestFixture]
     class ObjectExtensionsTests
     {
-        // TODO
+        [Test]
+        public void ShouldEqualsByReference()
+        {
+            Object first = new Object();
+            var second = first;
+
+            Check.That(Objects.Equal(first, second)).IsEqualTo(true);
+        }
+
+        [Test]
+        public void ShouldNotEqualTwoBareObjects()
+        {
+            Object first = new Object();
+            Object second = new Object();
+
+            Check.That(Objects.Equal(first, second)).IsEqualTo(false);
+        }
+
+        [Test]
+        public void ShouldEqualIComparable()
+        {
+
+        }
     }
 }
