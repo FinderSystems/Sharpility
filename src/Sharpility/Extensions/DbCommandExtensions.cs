83ba25e47eab9758ccc96208f92d2d7304dd0c67
Jacobus2k <jacobus2k.trashbin@gmail.com>
2015-06-11 11:18:06 +0200

    DbCommand extension (setParameter)

---
+++ src/Sharpility/Extensions/DbCommandExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Data;
+
+namespace Sharpility.Extensions
+{
+    /// <summary>
+    /// Extension of IDbCommand interface
+    /// </summary>
+    public static class DbCommandExtensions
+    {
+        public static void SetParameter(this IDbCommand command, string name, object value)
+        {
+            var parameter = command.CreateParameter();
+            parameter.ParameterName = name;
+            parameter.Value = value;
+            command.Parameters.Add(parameter);
+        }
+    }
+}
