739c8e31629f4ebe4466354418fc20a3beae1c8b
Jacobus2k <jacobus2k.trashbin@gmail.com>
2015-10-22 08:34:01 +0200

    Hashes from input streams

--- src/Sharpility.Tests/Util/HashesTests.cs
+++ src/Sharpility.Tests/Util/HashesTests.cs
@@ -1,5 +1,7 @@
-﻿using NFluent;
+﻿using System.IO;
+using NFluent;
 using NUnit.Framework;
+using Sharpility.Extensions;
 using Sharpility.Util;
 
 namespace Sharpility.Tests.Util
@@ -34,6 +36,21 @@ namespace Sharpility.Tests.Util
         }
 
         [Test]
+        public void ShouldReturnMd5HashFromStream()
+        {
+            // given
+            using (var stream = new MemoryStream("test".ToUtf8Bytes()))
+            {
+
+                // when
+                var checksum = Hashes.Md5(stream);
+
+                // then
+                Check.That(checksum).IsEqualTo("098f6bcd4621d373cade4e832627b4f6");
+            }
+        }
+
+        [Test]
         public void ShouldReturnNullSha1HashForNullString()
         {
             // given
@@ -60,6 +77,21 @@ namespace Sharpility.Tests.Util
         }
 
         [Test]
+        public void ShouldReturSha1HashFromStream()
+        {
+            // given
+            using (var stream = new MemoryStream("test".ToUtf8Bytes()))
+            {
+
+                // when
+                var checksum = Hashes.Sha1(stream);
+
+                // then
+                Check.That(checksum).IsEqualTo("a94a8fe5ccb19ba61c4c0873d391e987982fbbd3");
+            }
+        }
+
+        [Test]
         public void ShouldReturnNullSha256HashForNullString()
         {
             // given
@@ -83,6 +115,21 @@ namespace Sharpility.Tests.Util
 
             // then
             Check.That(checksum).IsEqualTo("9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08");
+        }
+
+        [Test]
+        public void ShouldReturSha256HashFromStream()
+        {
+            // given
+            using (var stream = new MemoryStream("test".ToUtf8Bytes()))
+            {
+
+                // when
+                var checksum = Hashes.Sha256(stream);
+
+                // then
+                Check.That(checksum).IsEqualTo("9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08");
+            }
         }
     }
 }
--- src/Sharpility/Util/Hashes.cs
+++ src/Sharpility/Util/Hashes.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 using Sharpility.Extensions;
@@ -28,6 +29,17 @@ namespace Sharpility.Util
         }
 
         /// <summary>
+        /// Generates MD5 hash in hex string.
+        /// </summary>
+        /// <param name="inputStream">Input stream to hash</param>
+        /// <returns>MD5 hash</returns>
+        public static string Md5(Stream inputStream)
+        {
+            var hash = MD5.Create().ComputeHash(inputStream);
+            return ToHexString(hash);
+        }
+
+        /// <summary>
         /// Generates SHA-1 hash in hex string.
         /// </summary>
         /// <param name="value">Hashed value</param>
@@ -45,6 +57,17 @@ namespace Sharpility.Util
         }
 
         /// <summary>
+        /// Generates SHA-1 hash in hex string.
+        /// </summary>
+        /// <param name="inputStream">Input stream to hash</param>
+        /// <returns>SHA-1 hash</returns>
+        public static string Sha1(Stream inputStream)
+        {
+            var hash = SHA1.Create().ComputeHash(inputStream);
+            return ToHexString(hash);
+        }
+
+        /// <summary>
         /// Generates SHA-256 hash in hex string.
         /// </summary>
         /// <param name="value">Hashed value</param>
@@ -58,6 +81,17 @@ namespace Sharpility.Util
             }
             var bytesToHash = value.ToBytes(encoding);
             var hash = SHA256.Create().ComputeHash(bytesToHash);
+            return ToHexString(hash);
+        }
+
+        /// <summary>
+        /// Generates SHA-256 hash in hex string.
+        /// </summary>
+        /// <param name="inputStream">Input stream to hash</param>
+        /// <returns>SHA-256 hash</returns>
+        public static string Sha256(Stream inputStream)
+        {
+            var hash = SHA256.Create().ComputeHash(inputStream);
             return ToHexString(hash);
         }
 
