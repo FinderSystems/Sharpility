6aeef2ad4c87b0c6be6c833d7af9026b7636817f
Jakub Malek <j.malek@findersystems.pl>
2016-02-09 12:46:13 +0100

    Small changes in AssemblyInfo

--- src/Sharpility/Properties/AssemblyInfo.cs
+++ src/Sharpility/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Sharpility")]
-[assembly: AssemblyDescription("C# commons")]
+[assembly: AssemblyDescription(".Net commons and extensions.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("FinderSystems")]
 [assembly: AssemblyProduct("Sharpility")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © FinderSystems 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
