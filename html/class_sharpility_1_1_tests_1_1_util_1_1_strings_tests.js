62291907cf9feef43c302185e72aa6f3f39610d3
Marcin Stefaniuk <marcin@stefaniuk.info>
2016-02-09 10:08:29 +0000

    Rebuild pages at d64cfc6

--- html/class_sharpility_1_1_tests_1_1_util_1_1_strings_tests.js
+++ html/class_sharpility_1_1_tests_1_1_util_1_1_strings_tests.js
@@ -5,5 +5,6 @@ var class_sharpility_1_1_tests_1_1_util_1_1_strings_tests =
     [ "ShouldReturnByteArrayAsString", "class_sharpility_1_1_tests_1_1_util_1_1_strings_tests.html#adaf25af886650bf820f71676b0c0b380", null ],
     [ "ShouldReturnLimitedString", "class_sharpility_1_1_tests_1_1_util_1_1_strings_tests.html#a930d70c63778344b3c32f8242dfa8bf8", null ],
     [ "ShouldReturnReturnObjectToString", "class_sharpility_1_1_tests_1_1_util_1_1_strings_tests.html#a08816989756cf10a53638647529d25d7", null ],
-    [ "ShouldReturnStringLength", "class_sharpility_1_1_tests_1_1_util_1_1_strings_tests.html#ac8bbe46512d0d575d4956075d6b6adea", null ]
+    [ "ShouldReturnStringLength", "class_sharpility_1_1_tests_1_1_util_1_1_strings_tests.html#ac8bbe46512d0d575d4956075d6b6adea", null ],
+    [ "ToStringOfDateTimeWithLocalKindShouldIncludeZoneOffset", "class_sharpility_1_1_tests_1_1_util_1_1_strings_tests.html#aafab4be85854b81235fb7c65e6897188", null ]
 ];
